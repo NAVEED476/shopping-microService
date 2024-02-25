3c2f9c07d5be256176783ffd4c92027ce7bc430b
Naveed Shaik <shaiknaveed435@gmail.com>
2024-02-25 20:22:33 +0530

    authorization

---
+++ routes/VerifyToken.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+    const token = req.params.token
+  if (authHeader) {
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) return res.status(401).json("token is inValid !");
+      req.user = user;
+    });
+  } else {
+    return res.status(403).json("you are not authenticated");
+  }
+};
+
+const verifyTokenAndAuthorization = (req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next();
+        }else{
+            res.status(403).json("you are not allowed to do that")
+        }
+    })
+}
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
+const { verifyTokenAndAuthorization } = require("./VerifyToken");
+
 const userRoute = require("express").Router();
 
-userRoute.get("/hello",(req,res)=>{
-    res.send("hello mawa")
+// UPDATE
+userRoute.put("/:id", verifyTokenAndAuthorization,(req,res)=>{
+
 })
-userRoute.post("/userdetails",async(req,res)=>{
-    let {name,email, number, password} = await req.body
-    res.json(name)
-})
+
 
 module.exports = userRoute;
