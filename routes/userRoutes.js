1f7a0f21066eff1de63a1ce43e77147f78c0dfb9
Naveed Shaik <shaiknaveed435@gmail.com>
2024-02-28 10:51:39 +0530

    user authrization completed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,15 @@
 const Users = require("../Models/userSchema");
-const { verifyTokenAndAuthorization } = require("./VerifyToken");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./VerifyToken");
 const CryptoJS = require("crypto-js");
 
 const userRoute = require("express").Router();
 
-
 // UPDATE
 userRoute.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   console.log("hello");
+  //   console.log("hello");
 
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
@@ -26,6 +28,40 @@ userRoute.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
       }
     );
     res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Delete
+userRoute.delete(
+  "/delete/:id",
+  verifyTokenAndAuthorization,
+  async (req, res) => {
+    try {
+      await Users.findByIdAndDelete(req.params.id);
+      res.status(200).json("user has been deleted successfully!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+);
+
+userRoute.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await Users.findById(req.params.id);
+
+    const { password, ...others } = user;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+userRoute.get("/users", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
   }
